b7027119c4037b4a80d9f99f520e701890796c28
Henrique Albuquerque <henrique_barretto@outlook.com>
2021-08-22 17:10:43 -0300

    feat: pagina about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AAAAAAAAAAAA';
+
+  ngOnInit(): void {}
+
+  goTo(section: string): void {
+    document.getElementById(section)?.scrollIntoView();
+  }
 }
