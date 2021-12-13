7bcef5d5d0e4a7aa5bef68ac1197cc040b3f866d
Henrique Albuquerque <henrique_barretto@outlook.com>
2021-12-12 22:03:34 -0300

    social links

--- src/app/components/socials/socials.component.ts
+++ src/app/components/socials/socials.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-socials',
   templateUrl: './socials.component.html',
-  styleUrls: ['./socials.component.sass']
+  styleUrls: ['./socials.component.sass'],
 })
 export class SocialsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openUrl(link: string): void {
+    window.open(link, '_blank');
   }
-
 }
